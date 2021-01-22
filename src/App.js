3553f590b8f501de07a31b82eccd69d64ca0b7a1
Horace J. Meng <hjmeng@tovide.com>
2021-01-22 15:56:08 -0800

    add App.js and index.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,8 @@
-//import logo from './logo.svg';
 import './App.css';
-import $ from 'jquery';
-
 
 function App() {
-/*
-    function copyIframeContent() {
-	var iframeContent = $(iframe).contents(); //alert(iframeContent);
-	//$("#result").text("Hello World");
-	$("#result").html(iframeContent.find('body').html);
-	alert(iframeContent.find('body').html());
-    }
-*/
-    return (
-	    <div className="App">
-	    <header className="App-header">
-	    </header>
-            <body>
-	    <h1>Hello World!</h1>
-	    <iframe title="heartrate" id="myIframe" name="myIframe"  allowusermedia width="1200" height="800" src="https://heartrateleaderboard.netlify.app/" allow="camera; microphone;"></iframe>
-	    Result: <br />
-	    <textarea id='result'></textarea>
-	    </body>
 
-	</div>
+    return (
     );
 }
 
